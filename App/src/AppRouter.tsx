115aa2a281166f39540d9ec5bd5f0efd687abfbe
shijin p <shijincht65@gmail.com>
2024-03-19 18:50:24 +0530

    day7 - ProtectedRoute & cloudinary setup

--- App/src/AppRouter.tsx
+++ App/src/AppRouter.tsx
@@ -3,13 +3,15 @@ import Layout from './Layout'
 import HomePage from './pages/HomePage'
 import AuthCallbackPage from './pages/AuthCallbackPage'
 import UserProfilePage from './pages/UserProfilePage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function AppRouter() {
   return (
     <Routes>
         <Route path='/' element={<Layout hero><HomePage/></Layout>}/>
         <Route path='/auth-callback' element={<AuthCallbackPage/>}/>
-        <Route path='/user-profile' element={<Layout><UserProfilePage/></Layout>}/>
+        <Route element={<ProtectedRoute/>}><Route path='/user-profile' element={<Layout><UserProfilePage/></Layout>}/></Route>
+        
         <Route path='*' element={<Navigate to={"/"}/>}/>
     </Routes>
   )
--- Server/src/index.ts
+++ Server/src/index.ts
@@ -3,21 +3,31 @@ import cors from 'cors'
 import 'dotenv/config'
 import mongoose from "mongoose";
 import UserController from "./routes/UserRoute";
+import {v2 as cloudinary} from 'cloudinary';
 
 const PORT = 4000 || process.env.PORT
 const server =express()
 server.use(express.json())
 server.use(cors())
 try {
- mongoose.connect(process.env.CONNECTION_STRING as string).then(()=>{console.log("Connected to DataBase")})
-   
+ mongoose.connect(process.env.CONNECTION_STRING as string).then(()=>{console.log("Connected to DataBase")}) 
 } catch (error) {
-  console.log("database connection failed",error)
-    
+  console.log("database connection failed",error)    
 }
-server.get("/test",async(req:Request ,res:Response)=>{
-    res.json({message:"hello it is from the server"})
+
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_SECRET_KEY, 
+});
+
+
+server.get("/health",async(req:Request ,res:Response)=>{
+    res.status(200).json({message:"HEALTH OK!"})
 })
+
+
 server.use("/api/my/user",UserController)
 
 server.listen(PORT,()=>{console.log(`server started at port : ${PORT}`)})
